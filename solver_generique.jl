a432072dccb45056043a6d51c867699c9071544e
kowdin <byakuya.dorian@yahoo.fr>
2016-11-18 20:31:11 +0100

    fonctionne mais lentement

--- solver_generique.jl
+++ solver_generique.jl
@@ -2,72 +2,90 @@ using JuMP
 using GLPKMathProgInterface
 using GLPK
 
-mSSCFLP = Model(solver=GLPKSolverMIP())
+#fichier a utiliser
+nomfile = [0,1#=,2,3,6,7,9,10,13,26,30,31,33=#]
 
-# --- Indices, donnees, variables ---
-x = []; y = [];
-@variable(mSSCFLP, x[1:7], Bin) #ouverture facilité
-@variable(mSSCFLP, y[1:20][1:7], Bin) #association
+for nom in nomfile
+    #lecture des donnees
+    f = open("./instances/p$(nom).txt")
+    tmp = split(readline(f)," ")
+    nbClients = parse(Int64, tmp[1])
+    nbDepos = parse(Int64, tmp[2])
 
-ouverture = [1,2,3,4,5,6,7]
-association[20][7]
-for i = 1:20
-    for j = 1:7
-        association[i][j] = 1
-    end
-end
-capacite = [5,5,5,5,5,5,5]
-demande = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-
-# --- Modèle à résoudre, résolution ---
-
-function doubleSomme(y, association, nbclient, nbdepos)
-    sum = 0
-    for i = 1:nbclient
-        for j = 1:nbdepos
-            sum = sum + y[i][j]*association[i][j]
+    association = collect(reshape(1:nbDepos*nbClients, nbClients, nbDepos)) #cout d'association
+    for i = 1:nbClients
+        tmp = split(readline(f)," ")
+        for j = 1:nbDepos
+            association[i,j] = parse(Int64, tmp[j])
         end
     end
-    return sum
-end
 
-function demandeDepos(y, demande, nbclient, numDepos)
-    sum = 0
-    for i = 1:nbclient
-        sum = sum + y[i][j]*demande[i][j]
+    tmp = split(readline(f)," ")
+    demande = []
+    for i = 1:nbClients
+        push!(demande, parse(Int64, tmp[i]))
     end
-    return sum
-end
 
-function demandeClient(y, nbdepos, numClient)
-    sum = 0
-    for j = 1:nbdepos
-        sum = sum + y[i][j]
+    tmp = split(readline(f)," ")
+    ouverture = []
+    for j = 1:nbDepos
+        push!(ouverture, parse(Int64, tmp[j]))
     end
-    return sum
-end
 
-@objective(mSSCFLP, Min, dot(ouverture, x) + doubleSomme(y, association, 20, 7))
-
-for j = 1:7
-    @constraint(mSSCFLP, demandeDepos(y, demande, 20, j) <= x[j]*capacite[j])
-end
-
-for i = 1:20
-    @constraint(mSSCFLP, demandeClient(y, 7, i) == 1)
-end
-
-status = solve(mSSCFLP)
-
-# Extraction des résultats ---
-println("z = : ", getobjectivevalue(mSSCFLP))
-for i = 1:20
-    for j = 1:7
-        print("$i ", getvalue(y[i][j]))
+    tmp = split(readline(f)," ")
+    capacite =[]
+    for j = 1:nbDepos
+        push!(capacite, parse(Int64, tmp[j]))
     end
-    print("\n")
-end
-println("\n")
-for j = 1:7
-    print("$i ", getvalue(x[j]))
+
+    println(nbClients)
+    println(nbDepos)
+    for i = 1:nbClients
+        for j = 1:nbDepos
+            print(association[i,j]," ")
+        end
+        print("\n")
+    end
+    println(demande)
+    println(ouverture)
+    println(capacite)
+
+
+    #declaration
+    mSSCFLP = Model(solver=GLPKSolverMIP())
+
+    #variables
+    @variable(mSSCFLP, x[1:nbDepos], Bin)
+    @variable(mSSCFLP, y[1:nbClients,1:nbDepos], Bin)
+
+    @objective(mSSCFLP, Min, sum(ouverture[j] * x[j] + sum( y[i,j] * association[i,j] for i=1:nbClients ) for j=1:nbDepos))
+
+    for j = 1:nbDepos
+        @constraint(mSSCFLP, sum(y[i,j] * demande[i] for i=1:nbClients) <= capacite[j]*x[j])
+    end
+    for i = 1:nbClients
+        @constraint(mSSCFLP, sum(y[i,j] for j=1:nbDepos) == 1)
+    end
+
+print(mSSCFLP)
+
+    tic()
+    status = solve(mSSCFLP)
+    time = toq();
+
+    #extraction des résultats
+    println("z = : ", getobjectivevalue(mSSCFLP))
+    for i = 1:nbClients
+        for j = 1:nbDepos
+            print(getvalue(y[i,j])," ")
+        end
+        print("\n")
+    end
+    println("\n")
+    for j = 1:nbDepos
+        print(getvalue(x[j])," ")
+    end
+    println("\n\n temps : ",time)
+    println("\n\n")
+
 end
