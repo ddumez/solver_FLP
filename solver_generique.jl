77cfdbeba35024e611072153981f977d51c89d75
kowdin <byakuya.dorian@yahoo.fr>
2016-11-19 15:33:22 +0100

    fonctionne avec CPLEX

--- solver_generique.jl
+++ solver_generique.jl
@@ -1,39 +1,41 @@
 using JuMP
 using GLPKMathProgInterface
-using GLPK
+using CPLEX
+
+function main()
 
 #fichier a utiliser
-nomfile = [0,1#=,2,3,6,7,9,10,13,26,30,31,33=#]
+nomfile = [0,1,2,3,6,7,9,10,13,26,30,31,33]
 
 for nom in nomfile
     #lecture des donnees
-    f = open("./instances/p$(nom).txt")
-    tmp = split(readline(f)," ")
-    nbClients = parse(Int64, tmp[1])
-    nbDepos = parse(Int64, tmp[2])
-
-    association = collect(reshape(1:nbDepos*nbClients, nbClients, nbDepos)) #cout d'association
+    f = open("./instances/p$(nom).txt")::IOStream
+    tmp = split(readline(f)," ")::Array
+    nbClients = parse(Int64, tmp[1])::Int64
+    nbDepos = parse(Int64, tmp[2])::Int64
+println("(",nbClients,";",nbDepos,")")
+    association = collect(reshape(1:nbDepos*nbClients, nbClients, nbDepos))::Array #cout d'association
     for i = 1:nbClients
-        tmp = split(readline(f)," ")
+        tmp = split(readline(f)," ")::Array
         for j = 1:nbDepos
-            association[i,j] = parse(Int64, tmp[j])
+            association[i,j] = parse(Int64, tmp[j])::Int64
         end
     end
 
-    tmp = split(readline(f)," ")
-    demande = []
+    tmp = split(readline(f)," ")::Array
+    demande = []::Array
     for i = 1:nbClients
         push!(demande, parse(Int64, tmp[i]))
     end
 
-    tmp = split(readline(f)," ")
+    tmp = split(readline(f)," ")::Array
     ouverture = []
     for j = 1:nbDepos
         push!(ouverture, parse(Int64, tmp[j]))
     end
 
-    tmp = split(readline(f)," ")
-    capacite =[]
+    tmp = split(readline(f)," ")::Array
+    capacite =[]::Array
     for j = 1:nbDepos
         push!(capacite, parse(Int64, tmp[j]))
     end
@@ -52,8 +54,7 @@ for nom in nomfile
 
 
     #declaration
-    mSSCFLP = Model(solver=GLPKSolverMIP())
-
+    mSSCFLP = Model(solver=CplexSolver())::Model
     #variables
     @variable(mSSCFLP, x[1:nbDepos], Bin)
     @variable(mSSCFLP, y[1:nbClients,1:nbDepos], Bin)
@@ -67,15 +68,13 @@ for nom in nomfile
         @constraint(mSSCFLP, sum(y[i,j] for j=1:nbDepos) == 1)
     end
 
-print(mSSCFLP)
+#println(mSSCFLP)
 
-    tic()
-    status = solve(mSSCFLP)
-    time = toq();
+    @time solve(mSSCFLP; suppress_warnings=true, relaxation=false)
 
     #extraction des résultats
     println("z = : ", getobjectivevalue(mSSCFLP))
-    for i = 1:nbClients
+#=    for i = 1:nbClients
         for j = 1:nbDepos
             print(getvalue(y[i,j])," ")
         end
@@ -84,8 +83,11 @@ print(mSSCFLP)
     println("\n")
     for j = 1:nbDepos
         print(getvalue(x[j])," ")
-    end
-    println("\n\n temps : ",time)
+    end=#
     println("\n\n")
 
 end
+
+end #du main
+
+main()
